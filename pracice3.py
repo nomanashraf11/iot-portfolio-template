0ecadff970a8f4105a5f9f8595c221f21fe269dd
Noman  Ashraf <110373764+nomanashraf11@users.noreply.github.com>
2025-06-22 23:12:54 +0300

    REMOVED COMMENTS

--- pracice3.py
+++ pracice3.py
@@ -7,12 +7,10 @@ class Item:
         print(f"{self.name}, {self.weight} kg")  # Output the item's name and weight
 
 
-# Create three items
 item1 = Item("Laptop", 2)
 item2 = Item("Book", 1)
 item3 = Item("Dumbbell", 4)
 
-# Output the items
 item1.output_item()
 item2.output_item()
 item3.output_item()
